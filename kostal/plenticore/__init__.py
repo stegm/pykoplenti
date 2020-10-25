25d0438d16363d78707983b2d4262cc0ca4de518
Stefan Gmeiner <stefangm42@gmail.com>
2020-10-25 19:59:05 +0100

    Import

---
+++ kostal/plenticore/__init__.py
@@ -0,0 +1,207 @@
+from aiohttp import ClientSession, ClientResponse
+from base64 import b64encode, b64decode
+from yarl import URL
+from hashlib import pbkdf2_hmac
+from Crypto.Cipher import AES
+import hmac
+import hashlib
+from os import urandom
+from typing import Iterable
+from json import dumps
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+class MeData:
+    """Represent the data of the 'me'-request."""
+    def __init__(self, raw):
+        self._raw = raw
+
+    @property
+    def is_locked(self) -> bool:
+        return self._raw['locked']
+
+    @property
+    def is_active(self) -> bool:
+        return self._raw['active']
+
+    @property
+    def is_authenticated(self) -> bool:
+        return self._raw['authenticated']
+
+    @property
+    def permissions(self) -> Iterable[str]:
+        return self._raw['permissions']
+
+    @property
+    def is_anonymous(self) -> bool:
+        return self._raw['anonymous']
+
+    @property
+    def role(self) -> str:
+        return self._raw['role']
+
+    def __str__(self):
+        return f'Me(locked={self.is_locked}, active={self.is_active}, authenticated={self.is_authenticated}, ' \
+               f'permissions={str(self.permissions)} anonymous={self.is_anonymous} role={self.role})'
+
+    def __repr__(self):
+        return dumps(self._raw)
+
+class ModuleData:
+    """Represents a single module."""
+    def __init__(self, raw):
+        self._raw = raw
+
+    @property
+    def id(self) -> str:
+        return self._raw['id']
+
+    @property
+    def type(self) -> str:
+        return self._raw['type']
+
+    def __str__(self):
+        return f'Module(id={self.id}, type={self.type})'
+
+    def __repr__(self):
+        return dumps(self._raw)
+
+class ProcessData:
+    """Represents a single process data."""
+    def __init__(self, raw):
+        self._raw = raw
+
+    @property
+    def id(self) -> str:
+        return self._raw['id']
+
+    @property
+    def unit(self) -> str:
+        return self._raw['unit']
+
+    @property
+    def value(self) -> float:
+        return self._raw['value']
+
+    def __str__(self):
+        return f'ProcessData(id={self.id}, unit={self.unit}, value={self.value})'
+
+    def __repr__(self):
+        return dumps(self._raw)
+
+
+class PlenticoreClient:
+    """Client for REST-API of plenticore inverters."""
+
+    BASE_URL = '/api/v1/'
+
+    def __init__(self, websession: ClientSession, host: str, port: int = 80):
+        self.websession = websession
+        self.host = host
+        self.port = port
+        self.session_id = None
+
+    def _create_url(self, path: str) -> URL:
+        """Creates a REST-API URL with the given path as suffix."""
+        base = URL.build(scheme='http', host=self.host, port=self.port, path=PlenticoreClient.BASE_URL)
+        return base.join(URL(path))
+
+    async def login(self, password: str, user: str = 'user'):
+        # Step 1 start authentication
+        client_nonce = urandom(12)
+
+        start_request = {
+            "username": user,
+            "nonce": b64encode(client_nonce).decode('utf-8')
+        }
+
+        async with self.websession.request(
+            "POST", self._create_url('auth/start'), json=start_request
+        ) as resp:
+            start_response = await resp.json()
+            server_nonce = b64decode(start_response['nonce'])
+            transaction_id = b64decode(start_response['transactionId'])
+            salt = b64decode(start_response['salt'])
+            rounds = start_response['rounds']
+
+        # Step 2 finish authentication (RFC5802)
+        salted_passwd = pbkdf2_hmac('sha256', password.encode('utf-8'), salt, rounds)
+        client_key = hmac.new(salted_passwd, 'Client Key'.encode('utf-8'), hashlib.sha256).digest()
+        stored_key = hashlib.sha256(client_key).digest()
+
+        auth_msg = 'n={user},r={client_nonce},r={server_nonce},s={salt},i={rounds},c=biws,r={server_nonce}'.format(
+            user=user,
+            client_nonce=b64encode(client_nonce).decode('utf-8'),
+            server_nonce=b64encode(server_nonce).decode('utf-8'),
+            salt=b64encode(salt).decode('utf-8'),
+            rounds=rounds
+        )
+        client_signature = hmac.new(stored_key, auth_msg.encode('utf-8'), hashlib.sha256).digest()
+        client_proof = bytes([a ^ b for a, b in zip(client_key, client_signature)])
+
+        server_key = hmac.new(salted_passwd, 'Server Key'.encode('utf-8'), hashlib.sha256).digest()
+        server_signature = hmac.new(server_key, auth_msg.encode('utf-8'), hashlib.sha256).digest()
+
+        finish_request = {
+            "transactionId": b64encode(transaction_id).decode('utf-8'),
+            "proof": b64encode(client_proof).decode('utf-8')
+        }
+
+        async with self.websession.request(
+            "POST", self._create_url('auth/finish'), json=finish_request
+        ) as resp:
+            finish_response = await resp.json()
+            token = finish_response['token']
+            signature = b64decode(finish_response['signature'])
+            if signature != server_signature:
+                raise Exception('Server signature mismatch.')
+
+        # Step 3 create session
+        session_key_hmac = hmac.new(stored_key, 'Session Key'.encode('utf-8'), hashlib.sha256)
+        session_key_hmac.update(auth_msg.encode('utf-8'))
+        session_key_hmac.update(client_key)
+        protocol_key = session_key_hmac.digest()
+        session_nonce = urandom(16)
+        cipher = AES.new(protocol_key, AES.MODE_GCM, nonce=session_nonce)
+        cipher_text, auth_tag = cipher.encrypt_and_digest(token.encode('utf-8'))
+
+        session_request = {
+            'transactionId': b64encode(transaction_id).decode('utf-8'),
+            'iv': b64encode(session_nonce).decode('utf-8'),
+            'tag': b64encode(auth_tag).decode('utf-8'),
+            'payload': b64encode(cipher_text).decode('utf-8'),
+        }
+
+        async with self.websession.request(
+            "POST", self._create_url('auth/create_session'), json=session_request
+        ) as resp:
+            session_response = await resp.json()
+            self.session_id = session_response['sessionId']
+
+    def _session_request(self, path: str, method='GET'):
+        """Make an request on the current active session."""
+        # TODO exception if session does not exist
+        headers = { 'authorization': f'Session {self.session_id}'}
+        return self.websession.request(method, self._create_url(path), headers=headers)
+
+    async def get_me(self) -> MeData:
+        """Returns information about the user."""
+        async with self._session_request('auth/me') as resp:
+            me_response = await resp.json()
+            return MeData(me_response)
+
+    async def get_modules(self) -> Iterable[ModuleData]:
+        """Returns list of all available modules (providing process data or settings)."""
+        async with self._session_request('modules') as resp:
+            modules_response = await resp.json()
+            return list([ModuleData(x) for x in modules_response])
+
+    async def get_process_data(self, module_id: str) -> Iterable[ProcessData]:
+        """Returns list of all available process-data identifiers of a module."""
+        async with self._session_request(f'processdata/{module_id}') as resp:
+            data_response = await resp.json()
+            return list([ProcessData(x) for x in data_response[0]['processdata']])
+
---
+++ kostal/plenticore/cli.py
@@ -0,0 +1,145 @@
+from kostal.plenticore import PlenticoreClient
+import asyncio
+from aiohttp import ClientSession
+import argparse
+import getpass
+from prompt_toolkit import PromptSession, print_formatted_text
+import os
+import tempfile
+
+
+class SessionCache:
+    """Persistent the session in a temporary file."""
+    def __init__(self, host):
+        self.host = host
+
+    def read_session_id(self) -> str:
+        file = os.path.join(tempfile.gettempdir(), f'plenticore-session-{self.host}')
+        if os.path.isfile(file):
+            with open(file, 'rt') as f:
+                return f.readline(256)
+        else:
+            return None
+
+    def write_session_id(self, id: str):
+        file = os.path.join(tempfile.gettempdir(), f'plenticore-session-{self.host}')
+        with os.open(file, os.O_WRONLY or os.O_TRUNC, mode=0o600) as f:
+            f.write(id)
+
+
+
+class PlenticoreShell:
+    """Provides a shell-like access to the plenticore client."""
+    def __init__(self, client: PlenticoreClient):
+        super().__init__()
+        self.client = client
+        self._session_cache = SessionCache(self.client.host)
+
+    async def prepare_client(self, passwd):
+        # first try to reuse existing session
+        session_id = self._session_cache.read_session_id()
+        if session_id is not None:
+            self.client.session_id = session_id
+            print_formatted_text('Trying to reuse existing session... ', end=None)
+            me = await self.client.get_me()
+            if me.is_authenticated:
+                print_formatted_text('Success')
+                return
+            else:
+                print_formatted_text('Failed')
+
+        if passwd is not None:
+            print_formatted_text('Logging in... ', end=None)
+            await self.client.login(passwd)
+            self._session_cache.write_session_id(self.client.session_id)
+            print_formatted_text('Success')
+
+    async def run(self, passwd):
+        session = PromptSession()
+        print_formatted_text('') # Initialize output
+
+        await self.prepare_client(passwd)
+
+        while True:
+            try:
+                text = await session.prompt_async('(plenticore)> ')
+
+                if text.strip().lower() == 'exit':
+                    raise EOFError()
+                else:
+                    cmd, *args = text.strip().split()
+                    cmd = cmd.lower()
+                    try:
+                        func = getattr(self, 'do_' + cmd)
+                    except AttributeError:
+                        print_formatted_text(f'Unknown command: {cmd}')
+                    else:
+                        await func(*args)
+
+
+
+            except KeyboardInterrupt:
+                continue
+            except EOFError:
+                break
+
+    async def do_me(self, *args):
+        """Returns information about the current user."""
+        me = await self.client.get_me()
+        print_formatted_text(me)
+
+    async def do_login(self, *args):
+        if len(args) == 0:
+            passwd = getpass.getpass()
+        else:
+            passwd = args[0]
+
+        await self.client.login(passwd)
+        self._session_cache.write_session_id(self.client.session_id)
+
+    async def do_modules(self, *args):
+        modules = await self.client.get_modules()
+        print_formatted_text(*modules, sep='\n')
+
+    async def do_processdata(self, *args):
+        if len(args) == 0:
+            print_formatted_text("At least one module must be given")
+            return
+
+        for module_id in args:
+            process_data = await self.client.get_process_data(module_id=module_id)
+            print_formatted_text(*process_data, sep='\n')
+
+
+
+async def main(host, port, passwd):
+    async with ClientSession() as session:
+        client = PlenticoreClient(session, host=host, port=port)
+
+        shell = PlenticoreShell(client)
+        await shell.run(passwd)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Plenticore command line client.')
+    parser.add_argument('--host', type=str, required=True,
+                        help='hostname or ip of plenticore inverter')
+    parser.add_argument('--port', type=int, default=80,
+                        help='port of plenticore (default 80)')
+    parser.add_argument('--password', type=str, default=None,
+                        help='password')
+    parser.add_argument('--password-file', type=str, default='secrets',
+                        help='password file (default "secrets" in the current working directory)')
+
+    args = parser.parse_args()
+
+    if args.password is not None:
+        passwd = args.password
+    elif os.path.isfile(args.password_file):
+        with open(args.password_file, 'rt') as f:
+            passwd = f.readline()
+    else:
+        passwd = None
+
+
+    asyncio.run(main(args.host, args.port, passwd))
